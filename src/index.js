55d4b15062960a4d1b501737cec5773632e43d62
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-11-06 19:27:52 -0500

    Start writing fetch fns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles/main.css';
+
+const searchBar = document.querySelector('#search-bar');
+const tempScale = document.querySelector('#temp-scale');
+const currentCity = document.querySelector('.city');
+const currentIcon = document.querySelector('current-icon');
+const currentTemp = document.querySelector('current-temp');
+const funStuff = [
+	'https://api.weatherapi.com/v1/current.json?k',
+	'ey=',
+	'043282325c',
+	'3b44ec9141',
+	'0010230511',
+	'&q=',
+].join('');
+
+async function getWeatherData() {
+	const targetCity = searchBar.value;
+	const response = await fetch(funStuff + targetCity, { mode: 'cors' });
+	const weatherData = await response.json();
+	return weatherData;
+}
